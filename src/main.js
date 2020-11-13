8e5748f8cb59b050645693c0df51d8df9946cb8a
Caroline-Jeldres <cjeldresm@gmail.com>
2020-11-13 13:48:58 -0300

    Se aplican cambios a css y se agrega font

--- src/main.js
+++ src/main.js
@@ -1,23 +1,3 @@
-import data from './data/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-<<<<<<< HEAD
-let footer= document.getElementById("footer")
-footer.innerHTML=`Todos los derechos reservados para Data lovers < L >`;
-document.getElementById("Pagtwo").onclick = function() {
-=======
-//let footer= document.getElementById("footer")
-//footer.innerHTML="Todos los derechos reservados para Data lovers < L >"
+// import data from './data/pokemon.js';
 
-document.getElementById("Pagtwo").onclick = function (){
->>>>>>> 2b9b6c44fd1318b98cc08ba60fa02c943bb6aca0
-    //function pastsecond() {
-        document.getElementById("Pagtwo").innerHTML = location.href='index2.html';
-    }
-// eslint-disable-next-line no-console
-console.log(data);
-
-document.getElementById("Pagtwo22").onclick = function retornoindice() {
-
-    document.getElementById("Pagtwo22").innerHTML = location.href='index.html'; 
-}
 
