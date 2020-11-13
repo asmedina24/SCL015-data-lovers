2a120ba3e290f22c68d57416d3a5f0f65f957a00
Caroline-Jeldres <cjeldresm@gmail.com>
2020-11-13 09:45:08 -0300

    sin cambios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import data from './data/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 let footer= document.getElementById("footer")
-footer.innerHTML="Todos los derechos reservados para Data lovers < L >"
+footer.innerHTML=`Todos los derechos reservados para Data lovers < L >`;
 document.getElementById("Pagtwo").onclick = function() {
     //function pastsecond() {
         document.getElementById("Pagtwo").innerHTML = location.href='index2.html';
