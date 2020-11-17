8a67d248af344d106179dba046d63adc0cd7a5c0
Caroline-Jeldres <cjeldresm@gmail.com>
2020-11-17 20:07:20 -0300

    funcion ordenar AZ

--- src/data.js
+++ src/data.js
@@ -1,9 +1,20 @@
-// estas funciones son de ejemplo
+import data  from './data/pokemon.js' // importar la data del data.js
 
-export const example = () => {
-  return 'example';
-};
+const funciones = {
+ordenarAZ: () => {    
+let pokemon = data.pokemon; // se exporta 
+//let listpokemon = pokemon.name
+pokemon.sort(function(a, b) {
+    if(a.name > b.name){
+        return 1;
+    }
+    if (a.name < b.name) {
+        return -1
+    }
+    //return 0;
+    });
+   
+}
+}
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export default funciones;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,40 @@
-// import data from './data/pokemon.js';
+import data from "./data/pokemon.js"
+import funciones from './data.js'
+let pokemon=data.pokemon;
+let pokelist=document.getElementById("content");
+const selectOrden=document.querySelector("#selectMenu");
+const optionsOrden=document.querySelector("#opmenu");
+const selectTypes=document.querySelector("#selectMenu2");
+const optionsTypes=document.querySelector("#opmenu2");
+const selectEggs=document.querySelector("#selectMenu3");
+const optionsEggs=document.querySelector("#opmenu3");
+const AZ=document.getElementById("AZ")
+
+
+selectOrden.addEventListener("click",()=>{
+    optionsOrden.classList.toggle("active");
+})
+selectTypes.addEventListener("click",()=>{
+    optionsTypes.classList.toggle("active");
+})
+selectEggs.addEventListener("click",()=>{
+    optionsEggs.classList.toggle("active");
+})
+function mostrarlista(){
+    pokelist.innerHTML=""
+    for(let i=0;i<pokemon.length; ++i){
+        pokelist.innerHTML+=(`<div class="pokelist">
+        <img class="pokeimg" src="${pokemon[i].img}"/>
+        <p class="namepoke">${pokemon[i].name}</p></div>`);
+    }
+}
+
+AZ.addEventListener("click",(event)=>{
+    event.preventDefault();
+    funciones.ordenarAZ(pokemon) 
+    mostrarlista()
+}
+)
+mostrarlista()
 
 
