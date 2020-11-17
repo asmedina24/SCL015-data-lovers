c34705511e965d4cb00ff96cf13d9da41a9a0b4b
alejandra-medina <alejandramedina1215@gmail.com>
2020-11-17 20:15:11 -0300

    Funcion sort

--- src/data.js
+++ src/data.js
@@ -1,9 +1,66 @@
-// estas funciones son de ejemplo
+import data  from './data/pokemon.js' // importar la data del data.js
 
-export const example = () => {
+const funciones = {
+ordenar: () => {    
+let pokemon = data.pokemon; // se exporta 
+//let listpokemon = pokemon.name
+pokemon.sort(function(a, b) {
+    if(a.name > b.name){
+        return 1;
+    }
+    if (a.name < b.name) {
+        return -1
+    }
+    //return 0;
+    });
+    //console.log (pokemon)
+let sortdata = pokemon.sort() 
+//console.log (sortdata)
+for (let i=0; i<sortdata.length; i++){
+// console.log (sortdata[i].name);
+}
+}
+}
+export default funciones;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log (pokemon);
+//onsole.log (pokemon[2].name);
+
+// imprime ordenado  a la z
+/*pokemon.sort(function(a, b) {
+    if(a.name > b.name){
+        return 1;
+    }
+
+    if (a.name < b.name) {
+        return -1
+    }
+
+    return 0;
+});
+
+console.log(pokemon);*/
+
+/*export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
 };
+*/
--- src/main.js
+++ src/main.js
@@ -1,3 +1,88 @@
-// import data from './data/pokemon.js';
+/* eslint-disable no-unused-vars */
+import data  from './data/pokemon.js'
+import funciones from './data.js'
+
+const pokemon = data.pokemon;
+//funciones.ordenar(pokemon) 
+//console.log(pokemon[i].name); 
+const conatiner = document.getElementById("mostrar");
+for (let i=0; i<pokemon.length; i++){
+conatiner.innerHTML += `<div>
+<p>${pokemon[i].name}</p>
+</div>`;
+}
 
 
+//funciones.ordenar(pokemon)
+
+let ordenaz = document.getElementById("mostraraz").addEventListener( "click" , function () {
+let borrar = "" 
+for (let i= 0; i<pokemon.length; i++)
+document.getElementById ("mostrar").innerHTML = `<div>
+<p>${pokemon[i].name}</p><img src= "${pokemon[i].img}"/>
+</div>`;
+    
+});
+
+
+
+
+
+
+
+
+
+/*let orderaz = document.getElementById("clickme");
+orderaz.addEventListener ("click", funcion myfuntion() {
+    if ("click" === true) {
+        //funciones.ordenar(pokemon) 
+//console.log(pokemon[i].name); 
+       // const conatiner = document.getElementById("mostrar");
+        for (let i=0; i<pokemon.length; i++){
+        conatiner.innerHTML += `<div>
+              <p>${pokemon[i].name}</p>
+        </div>`;
+} 
+}
+    else  {  for (let i=0; i<pokemon.length; i++){
+        conatiner.innerHTML += `<div>
+       <p>${pokemon[i].name}</p>
+        </div>`;
+        } 
+}
+});*/
+
+
+
+/*pokemon.sort(function(a, b) {
+    if(a.name > b.name){
+        return 1;
+    }
+
+    if (a.name < b.name) {
+        return -1
+    }
+
+    return 0;
+}));
+
+
+
+//console.log(sortdata[2].name); 
+
+
+
+/*import data  from './data/pokemon.js'
+import data1 from '/data.js'
+
+const pokemon = data.pokemon;
+let sortdata = pokemon.sort() 
+//console.log(pokemon[i].name); 
+const conatiner = document.getElementById("mostrar");
+for (let i=0; i<sortdata.length; i++){
+conatiner.innerHTML += `<div>
+<img src= "${pokemon[i].img}"/>
+<p>${sortdata[i].name}</p>
+</div>`;
+}
+//console.log(pokemon[i].name); */
