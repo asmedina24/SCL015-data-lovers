195b3d8f222414c9bc6e95324910f0e5589f476f
Caroline-Jeldres <cjeldresm@gmail.com>
2020-12-02 16:34:56 -0300

    cambios en el codigo

---
+++ src/contenedor.js
@@ -0,0 +1,84 @@
+let contruccion = false;
+const contenedor = {
+    prev_next: (prev_dev) => {
+        let pre_next = ''
+        // eslint-disable-next-line no-prototype-builtins
+        if (prev_dev.hasOwnProperty("next-evolution")) {
+            let evolucion = prev_dev["next-evolution"]
+            pre_next += '<div><strong>Evolución:</strong><span>'
+            for (let index = 0; index < evolucion.length; index++) {
+                pre_next += evolucion[index].name
+            }
+            pre_next += '</span></div>'
+        }
+
+        // eslint-disable-next-line no-prototype-builtins
+        if (prev_dev.hasOwnProperty("prev-evolution")) {
+            let evolucion = prev_dev["prev-evolution"]
+            pre_next += '<div class=modaldatos><strong>Pre-evolución:</strong><span>'
+            for (let index = 0; index < evolucion.length; index++) {
+                pre_next += evolucion[index].name
+            }
+            pre_next += '</span></div>'
+        }
+        return pre_next
+    },
+    mostrarlista: (pokemontemp) => {
+        let tiempo = 0
+        let pokelist = document.getElementById("content");
+        let pokemodal = document.getElementById("content_modal");
+        document.getElementById("loader").style.display = "block";
+        setTimeout(function () {
+            pokelist.innerHTML = "";
+            if (contruccion===false){
+                pokemodal.innerHTML = "";
+            }
+
+            for (let i = 0; i < pokemontemp.length; ++i) {
+                pokelist.innerHTML += (`<div id="${pokemontemp[i].num}"class="pokelist">
+                <img class="pokeimg" src="${pokemontemp[i].img}"/>
+                <p class="namepoke">${pokemontemp[i].name}</p></div>`);
+                if (contruccion===false){
+                    pokemodal.innerHTML += (`<div id="modal_${pokemontemp[i].num}" style="display:none;" class="modal">
+                            <div class="contenedor_modal">
+                            <div class="header_modal">
+                            <button type="button" class="close">X</button>
+                            </div>
+                            <div class="cuerpo_modal">
+                                <div class="modalimg"><img src="${pokemontemp[i].img}"/></div>
+                                <div class= "letrasdentromodal"><strong>Nombre:</strong><span>${pokemontemp[i].name}</span></div>
+                                <div class= "letrasdentromodal"><strong>Numero:</strong><span>${pokemontemp[i].num}</span></div>
+                                <div class= "letrasdentromodal"><strong>Generacion:</strong><span>${pokemontemp[i].generation.name}</span></div>
+                                <div class= "letrasdentromodal"><strong></strong><span>${pokemontemp[i].about}</span></div>
+                                <div class= "letrasdentromodal"><strong>Debilidad:</strong><span>${pokemontemp[i].weaknesses}</span></div>
+                                ${contenedor.prev_next(pokemontemp[i].evolution)}
+                            </div>
+                        </div>
+                    </div>`);
+                }
+                tiempo *= 1
+            }
+            document.getElementById("loader").style.display = "none";
+            contenedor.modal()
+            contruccion=true;
+        }, tiempo);
+
+    },
+    modal: () => {
+        let elements = document.getElementsByClassName("pokelist");
+        for (let i = 0; i < elements.length; i++) {
+            let elemento = elements[i]
+            elemento.addEventListener('click', function () {
+                document.getElementById('modal_' + elemento.id).style.display = "flex";
+            },false);
+        }
+        let modales = document.getElementsByClassName("close");
+        for (let i = 0; i < modales.length; i++) {
+            let elemento = modales[i]
+            elemento.addEventListener('click', function () {
+                elemento.closest('.modal').style.display = "none";
+            },false);
+        }
+    }
+}
+export default contenedor
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-// import data from "./data/pokemon.js"
 import funciones from './data.js'
+import contenedor from './contenedor.js'
 
 function getdata() {
     fetch('https://caroline-jeldres.github.io/SCL015-data-lovers/src/data/pokemon.json')
@@ -15,7 +15,7 @@ window.onload = getdata()
 
 function mostrarTodadata(data) {
     let pokemon = data.pokemon;
-    let pokemontemp = data.pokemon;
+    let pokemontemp = pokemon;
     const selectOrden = document.querySelector("#selectMenu");
     const optionsOrden = document.querySelector("#opmenu");
     const selectTypes = document.querySelector("#selectMenu2");
@@ -27,49 +27,63 @@ function mostrarTodadata(data) {
     const num = document.getElementById("1-251");
     const revenum = document.getElementById("251-1");
     let porcenType = document.getElementById("porcentaje");
-
-    funciones.mostrarlista(pokemontemp)
+    const quitar_filtro = document.getElementById("btn_quitar_filtro")
+    
+    
+    contenedor.mostrarlista(pokemontemp)
+    
 
     selectOrden.addEventListener("click", () => {
         optionsOrden.classList.toggle("active");
+        optionsEggs.classList.remove("active")
+        optionsTypes.classList.remove("active")
     })
     selectTypes.addEventListener("click", () => {
         optionsTypes.classList.toggle("active");
+        optionsOrden.classList.remove("active");
+        optionsEggs.classList.remove("active")
     })
     selectEggs.addEventListener("click", () => {
         optionsEggs.classList.toggle("active");
+        optionsTypes.classList.remove("active");
+        optionsOrden.classList.remove("active");
     })
-
+    
     // Funfiones AZ, ZA, 1 a 251, 251 a 1
 
     AZ.addEventListener("click", (event) => {
         event.preventDefault()
         funciones.ordenarAZ(pokemontemp)
         optionsOrden.classList.remove("active")
-        funciones.mostrarlista(pokemontemp)
-    }
-    )
+        contenedor.mostrarlista(pokemontemp)
+    })
     ZA.addEventListener("click", (event) => {
         event.preventDefault();
         funciones.ordenarZA(pokemontemp)
         optionsOrden.classList.remove("active")
-        funciones.mostrarlista(pokemontemp)
-    }
-    )
+        contenedor.mostrarlista(pokemontemp)
+     
+    })
     num.addEventListener("click", (event) => {
         event.preventDefault();
         funciones.ordenarnum(pokemontemp)
         optionsOrden.classList.remove("active")
-        funciones.mostrarlista(pokemontemp)
-    }
-    )
+        contenedor.mostrarlista(pokemontemp)
+   
+    })
     revenum.addEventListener("click", (event) => {
         event.preventDefault();
         funciones.revenum(pokemontemp)
         optionsOrden.classList.remove("active")
-        funciones.mostrarlista(pokemontemp)
-    }
-    )
+        contenedor.mostrarlista(pokemontemp)
+     
+    })
+    quitar_filtro.addEventListener("click", (event) => {
+        event.preventDefault();
+        contenedor.mostrarlista(pokemon)
+        porcenType.innerHTML = "";
+        pokemontemp = pokemon
+    })
     // Filtros por tipo y calculo por tipo
 
     let tipo_pokemon = document.getElementsByClassName("options");
@@ -82,8 +96,9 @@ function mostrarTodadata(data) {
             pokemontemp = funciones.filter(pokemon, condition)
             let pokemonPorcen = funciones.calculator(pokemon, condition)               ;
             optionsTypes.classList.remove("active")
-            porcenType.innerHTML = (`<p class="porcentaje">${condition} = ${pokemonPorcen} de la pokedex</p>`)
-            funciones.mostrarlista(pokemontemp)
+            porcenType.innerHTML = (`<p class="porcentaje">El tipo ${condition} corresponde a un ${pokemonPorcen} de la pokedex</p>`)
+            contenedor.mostrarlista(pokemontemp)
+            
         },
             false);
     }
@@ -93,19 +108,22 @@ function mostrarTodadata(data) {
     for (let i = 0; i < tipo_Huevos.length; i++) {
         let element = tipo_Huevos[i];
         element.addEventListener("click", function (event) {
+            porcenType.innerHTML = "";
             event.preventDefault();
             let conditionHuevo = element.id;
             pokemontemp = funciones.filterEgg(pokemon, conditionHuevo)
-            document.getElementById("opmenu3").classList.remove("active")
-            funciones.mostrarlista(pokemontemp)
+            optionsEggs.classList.remove("active")
+            contenedor.mostrarlista(pokemontemp)
+            
         })
     }
     // buscador interno
     let search = document.getElementById("search1");
     search.addEventListener("keyup", (text) => {
         let searchPokemon = text.target.value.toLowerCase() // variable igual al palabras igresadas x usuario en miniscula  
-        pokemontemp = funciones.search(pokemon, searchPokemon)
-        funciones.mostrarlista(pokemontemp)
+        contenedor.mostrarlista(contenedor.search(pokemontemp, searchPokemon))
+        
+        
     })
      
 }
